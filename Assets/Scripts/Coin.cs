932736a19850e66d8f0780b10403788e89818ffe
Brian <brianbtfa@hotmail.com>
2021-04-17 14:28:53 -0500

    Primer commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+
+    public static int coins_count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Coin.coins_count++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag ("Player"));
+            Destroy (gameObject);
+            
+    }
+
+    void OnDestroy()
+    {
+        Coin.coins_count--;
+        if(Coin.coins_count <= 0)
+        {
+            GameObject timer  = GameObject.Find ("GameTimer");  
+            Destroy (timer);
+            GameObject[] fire_works = GameObject.FindGameObjectsWithTag ("FireWork");
+            foreach (GameObject firework in fire_works){
+                firework.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float maxTime = 60.0f;
+    private float countdown = 0.0f;
+
+    void Start()
+    {
+        countdown = maxTime;
+    }
+
+    void Update()
+     {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0){
+               Coin.coins_count = 0;
+               Application.LoadLevel (Application.loadedLevel);
+        }
+    }
+}
